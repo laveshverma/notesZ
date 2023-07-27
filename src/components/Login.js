ddc77a34a9232997b06607dd39b37a5a73050579
LAVESH VERMA <70440150+laveshverma@users.noreply.github.com>
2023-07-27 07:08:17 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,8 @@
 import React, {useState}from 'react'
 import {useNavigate} from 'react-router-dom';
 
-const host = "https://tame-cyan-scarab-cape.cyclic.app"
+  //const host = "http://localhost:5000"
+    const host = "https://notesz-backend.onrender.com"
 function Login(props) {
     const [credentials, setCredentials] = useState({email:"", password:""});
    let navigate = useNavigate();
